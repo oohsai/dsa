228db04b221a620517c613b862bf9f58f619f1dc
Sai <92389813+oohsai@users.noreply.github.com>
2023-07-18 03:20:48 +0530

    Update Sieve of Eratosthenes.java

--- Maths/Sieve of Eratosthenes.java
+++ Maths/Sieve of Eratosthenes.java
@@ -1,15 +1,15 @@
 public class seive {
     public static void main(String[] args) {
-        int n = 6;
-        boolean[] primes = new boolean[n+1];
-        sieve(n, primes);
+        int n = 6; //chane this according to the number you want  to change to!
+        boolean[] primes = new boolean[n+1]; // Sets all array space to the n+1 size so that the number otself can be cosnidered and any boolean array is set to default false in java.
+        sieve(n, primes); 
     }
 
     static void sieve ( int n, boolean[] primes) {
-        for (int i = 2; i*i <= n; i++) {
-            if (!primes[i]){
-                for (int j = i*2; j <= n ; j+=i) {
-                    primes[j] = true;
+        for (int i = 2; i*i <= n; i++) { // as we dont need to check for numbers under the sqrt of the number, we use i*i to reduce the arr space.
+            if (!primes[i]){ // as all elements in primes array are set to false. everything at first passes this and goes on to the next loop
+                for (int j = i*2; j <= n ; j+=i) { //inner loop that check if the number is prime. vvvvvimp line
+                    primes[j] = true; //changes the value of the index to true
 
                 }
             }
@@ -17,8 +17,8 @@ public class seive {
         }
 
         for (int i = 2; i <= n; i++) {
-            if (!primes[i]) {
-                System.out.print(i + " ");
+            if (!primes[i]) { //true element gets printed i.e. element which has a factor is set to true and gets eliminated.
+                System.out.print(i + " "); //Print the series of Primes Numbers before the given number.
             }
 
         }
